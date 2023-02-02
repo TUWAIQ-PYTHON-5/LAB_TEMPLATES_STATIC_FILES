2d7ffc9ff2a977f00ba853c536ee7ea8074aa152
Mohammed <al5aldi.jav@gmail.com>
2023-02-02 06:37:25 +0300

    edit url

--- Tourister/CityApp/urls.py
+++ Tourister/CityApp/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("city/Riyadh/", views.Riyadh_page, name="Riyadh_page"),
     path("city/Abha/", views.Abha_page, name="Abha_page"),
     path("city/Mekkah/", views.Makkah_page, name="Makkah_page"),
-    path("city/Alula/", views.Alula_page, name="AlUla_page"),
+    path("city/Alula/", views.Alula_page, name="AlUla_page")
 
 ]
